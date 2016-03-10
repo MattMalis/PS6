4b9ab3fc0a9610ea5bb6a77b0ce0a38cd20c296f
MattMalis <iramalis@nah.dhcp.wustl.edu>
2016-03-10 16:02:43 -0600

    First commit

---
+++ Candidate.R
@@ -0,0 +1,15 @@
+getwd()
+setwd("/Users/iramalis/Desktop/gitstuff/PS6")
+
+#'A Candidate object
+#'
+#'Some more info on objects of the class \code{Candidate}, if you'd like
+#'
+#'\itemize{
+#'\item \code{name} The candidate's name
+#'\item \code{delegatesWon} Number of delegates the candidate has won to date
+#'\item \code{party} Candidate's party
+#'\item \code{delegatesNeeded} Number of delegates needed to secure party nomination
+#'}
+#'
+#'
